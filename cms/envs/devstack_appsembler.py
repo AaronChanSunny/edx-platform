2e802c920b4c757ff96a47b8f6f831abd5335729
Anna Callahan <annacallahan@gmail.com>
2015-12-14 17:21:22 -0500

    needed for migration

--- cms/envs/devstack_appsembler.py
+++ cms/envs/devstack_appsembler.py
@@ -3,8 +3,9 @@
 from .devstack import *
 from .appsembler import *
 
-INSTALLED_APPS += ('appsembler',)
-TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.context_processors.intercom',)
+INSTALLED_APPS += ('appsembler_lms',)
+# TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.context_processors.intercom',)
 
 FEATURES['APPSEMBLER_SECRET_KEY'] = "secret_key"
 OAUTH_ENFORCE_SECURE = False
+
